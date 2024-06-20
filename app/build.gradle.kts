a92c93c84cbb09a5f1f80d3a3f29e207ddbd2c37
José Darci Rodrigues Junior <juniorinternet@gmail.com>
2024-06-20 16:02:38 -0300

    ajustes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         targetSdk = 33
         versionCode = 3
         versionName = "1.3"
-
+        //desenvolvido em 20/06/2024
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
